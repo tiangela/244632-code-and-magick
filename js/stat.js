ed95c15f03253ba37c6281d0a0004c1c4b222592
Tiboeva Angela <tas14@yandex.ru>
2017-11-21 18:35:26 +0300

    Функции попытка - 2

--- js/stat.js
+++ js/stat.js
@@ -1,51 +1,55 @@
 'use strict';
 
 window.renderStatistics = function (ctx, names, times) {
-  ctx.fillStyle = 'rgba(0, 0, 0, 0.7)'; // black
-  ctx.fillRect(110, 20, 420, 270);
-  ctx.fillStyle = 'rgba(255, 255, 255, 1.0)'; // white;
-  ctx.strokeRect(100, 10, 420, 270);
-  ctx.fillRect(100, 10, 420, 270);
-
-
-  // black;
-  ctx.fillStyle = '#000';
-  ctx.font = '16px PT Mono';
-
-  ctx.fillText('Ура вы победили!', 120, 40);
-  ctx.fillText('Список результатов:', 120, 60);
-
   var barHeigth = 20; // px;
   var initialX = 150; // px;
   var indent = 90; // px;
   var initialY = 240; // px;
   var lineWitdth = 40; // px;
-  var maxTime = -1;
 
-var getMaxElement = function () {
-
-  for (var i = 0; i < times.length; i++) { // поиск максимального элемента массива
-    var time = times[i];
-    if (time > maxTime) {
-      maxTime = time;
-    }
-  }
-  return maxTime;
+  var colorResultField = function () {
+    ctx.fillStyle = 'rgba(0, 0, 0, 0.7)'; // black
+    ctx.fillRect(110, 20, 420, 270);
+    ctx.fillStyle = 'rgba(255, 255, 255, 1.0)'; // white;
+    ctx.strokeRect(100, 10, 420, 270);
+    ctx.fillRect(100, 10, 420, 270);
   };
+  colorResultField();
+  // black;
+  var textResult = function () {
+    ctx.fillStyle = '#000';
+    ctx.font = '16px PT Mono';
+
+    ctx.fillText('Ура вы победили!', 120, 40);
+    ctx.fillText('Список результатов:', 120, 60);
+  };
+  textResult();
+
+  var getMaxElement = function (times) {
+    var maxTime = -1;
+    for (var i = 0; i < times.length; i++) { // поиск максимального элемента массива
+      var time = times[i];
+      if (time > maxTime) {
+        maxTime = time;
+      }
+    }
+    return maxTime;
+  };
+  var maxTime = getMaxElement(times);
 
   var histogramHeigth = -150; // px;
   var step = histogramHeigth / maxTime; // px; рассчитывает пропорции
 
-var getColumns = function () {
-    for (i = 0; i < times.length; i++) { // перебирает массив ставит столбики имена и время
-    ctx.fillStyle = names[i] === 'Вы' ? 'red' : 'rgba(0, 0, 255, ' + getRandomValue(0.3, 1) + ')';
-    ctx.fillRect(initialX + indent * i, initialY, lineWitdth, times[i] * step);
-    ctx.fillStyle = 'black';
-    ctx.fillText(names[i], initialX + indent * i, initialY + barHeigth);
-    ctx.fillText(Math.floor(times[i]), initialX + indent * i, histogramHeigth + initialY - barHeigth / 2);
-  }
-  return ?;
-};
+  var getColumns = function () {
+    for (var i = 0; i < times.length; i++) { // перебирает массив ставит столбики имена и время
+      ctx.fillStyle = names[i] === 'Вы' ? 'red' : 'rgba(0, 0, 255, ' + getRandomValue(0.3, 1) + ')';
+      ctx.fillRect(initialX + indent * i, initialY, lineWitdth, times[i] * step);
+      ctx.fillStyle = 'black';
+      ctx.fillText(names[i], initialX + indent * i, initialY + barHeigth);
+      ctx.fillText(Math.floor(times[i]), initialX + indent * i, histogramHeigth + initialY - barHeigth / 2);
+    }
+  };
+  getColumns();
 
   function getRandomValue(minRandom, maxRandom) {
     return Math.random() * (maxRandom - minRandom) + minRandom;
